c5f7d34f14d0e427e3dca4b707c06f3a3707c5a4
btabilo <btabilosk8@gmail.com>
2017-03-08 00:34:16 -0300

    Se agregan los resumenes de las mesas dinamicamente

--- ARIAKA.SMARTGOURMET.Win/Controls/ResumenMesaControl.Designer.vb
+++ ARIAKA.SMARTGOURMET.Win/Controls/ResumenMesaControl.Designer.vb
@@ -22,10 +22,8 @@ Partial Class ResumenMesaControl
         Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
         Me.LabelControl_Hora_Value = New DevExpress.XtraEditors.LabelControl()
         Me.LabelControl_Garzon_Value = New DevExpress.XtraEditors.LabelControl()
-        Me.LabelControl_Total_Value = New DevExpress.XtraEditors.LabelControl()
         Me.LabelControl_MesaN_Value = New DevExpress.XtraEditors.LabelControl()
         Me.LabelControl_Numero = New DevExpress.XtraEditors.LabelControl()
-        Me.LabelControl_Total = New DevExpress.XtraEditors.LabelControl()
         Me.LabelControl_Garzon = New DevExpress.XtraEditors.LabelControl()
         Me.LabelControl_Hora = New DevExpress.XtraEditors.LabelControl()
         Me.TableLayoutPanel1.SuspendLayout()
@@ -36,22 +34,20 @@ Partial Class ResumenMesaControl
         Me.TableLayoutPanel1.ColumnCount = 2
         Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
         Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-        Me.TableLayoutPanel1.Controls.Add(Me.LabelControl_Hora_Value, 1, 3)
-        Me.TableLayoutPanel1.Controls.Add(Me.LabelControl_Garzon_Value, 1, 2)
-        Me.TableLayoutPanel1.Controls.Add(Me.LabelControl_Total_Value, 1, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.LabelControl_Hora_Value, 1, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.LabelControl_Garzon_Value, 1, 1)
         Me.TableLayoutPanel1.Controls.Add(Me.LabelControl_MesaN_Value, 1, 0)
         Me.TableLayoutPanel1.Controls.Add(Me.LabelControl_Numero, 0, 0)
-        Me.TableLayoutPanel1.Controls.Add(Me.LabelControl_Total, 0, 1)
-        Me.TableLayoutPanel1.Controls.Add(Me.LabelControl_Garzon, 0, 2)
-        Me.TableLayoutPanel1.Controls.Add(Me.LabelControl_Hora, 0, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.LabelControl_Garzon, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.LabelControl_Hora, 0, 2)
         Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
         Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
-        Me.TableLayoutPanel1.RowCount = 4
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel1.RowCount = 3
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
         Me.TableLayoutPanel1.Size = New System.Drawing.Size(182, 173)
         Me.TableLayoutPanel1.TabIndex = 0
         '
@@ -59,7 +55,7 @@ Partial Class ResumenMesaControl
         '
         Me.LabelControl_Hora_Value.Anchor = System.Windows.Forms.AnchorStyles.Left
         Me.LabelControl_Hora_Value.Appearance.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LabelControl_Hora_Value.Location = New System.Drawing.Point(94, 141)
+        Me.LabelControl_Hora_Value.Location = New System.Drawing.Point(94, 134)
         Me.LabelControl_Hora_Value.Name = "LabelControl_Hora_Value"
         Me.LabelControl_Hora_Value.Size = New System.Drawing.Size(0, 19)
         Me.LabelControl_Hora_Value.TabIndex = 7
@@ -68,25 +64,16 @@ Partial Class ResumenMesaControl
         '
         Me.LabelControl_Garzon_Value.Anchor = System.Windows.Forms.AnchorStyles.Left
         Me.LabelControl_Garzon_Value.Appearance.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LabelControl_Garzon_Value.Location = New System.Drawing.Point(94, 98)
+        Me.LabelControl_Garzon_Value.Location = New System.Drawing.Point(94, 76)
         Me.LabelControl_Garzon_Value.Name = "LabelControl_Garzon_Value"
         Me.LabelControl_Garzon_Value.Size = New System.Drawing.Size(0, 19)
         Me.LabelControl_Garzon_Value.TabIndex = 6
-        '
-        'LabelControl_Total_Value
-        '
-        Me.LabelControl_Total_Value.Anchor = System.Windows.Forms.AnchorStyles.Left
-        Me.LabelControl_Total_Value.Appearance.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LabelControl_Total_Value.Location = New System.Drawing.Point(94, 55)
-        Me.LabelControl_Total_Value.Name = "LabelControl_Total_Value"
-        Me.LabelControl_Total_Value.Size = New System.Drawing.Size(0, 19)
-        Me.LabelControl_Total_Value.TabIndex = 5
         '
         'LabelControl_MesaN_Value
         '
         Me.LabelControl_MesaN_Value.Anchor = System.Windows.Forms.AnchorStyles.Left
         Me.LabelControl_MesaN_Value.Appearance.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LabelControl_MesaN_Value.Location = New System.Drawing.Point(94, 12)
+        Me.LabelControl_MesaN_Value.Location = New System.Drawing.Point(94, 19)
         Me.LabelControl_MesaN_Value.Name = "LabelControl_MesaN_Value"
         Me.LabelControl_MesaN_Value.Size = New System.Drawing.Size(0, 19)
         Me.LabelControl_MesaN_Value.TabIndex = 4
@@ -95,27 +82,17 @@ Partial Class ResumenMesaControl
         '
         Me.LabelControl_Numero.Anchor = System.Windows.Forms.AnchorStyles.Right
         Me.LabelControl_Numero.Appearance.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LabelControl_Numero.Location = New System.Drawing.Point(15, 12)
+        Me.LabelControl_Numero.Location = New System.Drawing.Point(15, 19)
         Me.LabelControl_Numero.Name = "LabelControl_Numero"
         Me.LabelControl_Numero.Size = New System.Drawing.Size(73, 19)
         Me.LabelControl_Numero.TabIndex = 0
         Me.LabelControl_Numero.Text = "Mesa N°:"
         '
-        'LabelControl_Total
-        '
-        Me.LabelControl_Total.Anchor = System.Windows.Forms.AnchorStyles.Right
-        Me.LabelControl_Total.Appearance.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LabelControl_Total.Location = New System.Drawing.Point(39, 55)
-        Me.LabelControl_Total.Name = "LabelControl_Total"
-        Me.LabelControl_Total.Size = New System.Drawing.Size(49, 19)
-        Me.LabelControl_Total.TabIndex = 1
-        Me.LabelControl_Total.Text = "Total:"
-        '
         'LabelControl_Garzon
         '
         Me.LabelControl_Garzon.Anchor = System.Windows.Forms.AnchorStyles.Right
         Me.LabelControl_Garzon.Appearance.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LabelControl_Garzon.Location = New System.Drawing.Point(24, 98)
+        Me.LabelControl_Garzon.Location = New System.Drawing.Point(24, 76)
         Me.LabelControl_Garzon.Name = "LabelControl_Garzon"
         Me.LabelControl_Garzon.Size = New System.Drawing.Size(64, 19)
         Me.LabelControl_Garzon.TabIndex = 2
@@ -125,7 +102,7 @@ Partial Class ResumenMesaControl
         '
         Me.LabelControl_Hora.Anchor = System.Windows.Forms.AnchorStyles.Right
         Me.LabelControl_Hora.Appearance.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LabelControl_Hora.Location = New System.Drawing.Point(43, 141)
+        Me.LabelControl_Hora.Location = New System.Drawing.Point(43, 134)
         Me.LabelControl_Hora.Name = "LabelControl_Hora"
         Me.LabelControl_Hora.Size = New System.Drawing.Size(45, 19)
         Me.LabelControl_Hora.TabIndex = 3
@@ -150,9 +127,7 @@ Partial Class ResumenMesaControl
     Friend WithEvents LabelControl_Numero As DevExpress.XtraEditors.LabelControl
     Friend WithEvents LabelControl_Hora_Value As DevExpress.XtraEditors.LabelControl
     Friend WithEvents LabelControl_Garzon_Value As DevExpress.XtraEditors.LabelControl
-    Friend WithEvents LabelControl_Total_Value As DevExpress.XtraEditors.LabelControl
     Friend WithEvents LabelControl_MesaN_Value As DevExpress.XtraEditors.LabelControl
-    Friend WithEvents LabelControl_Total As DevExpress.XtraEditors.LabelControl
     Friend WithEvents LabelControl_Garzon As DevExpress.XtraEditors.LabelControl
     Friend WithEvents LabelControl_Hora As DevExpress.XtraEditors.LabelControl
 End Class
--- ARIAKA.SMARTGOURMET.Win/Controls/ResumenMesaViewControl.vb
+++ ARIAKA.SMARTGOURMET.Win/Controls/ResumenMesaViewControl.vb
@@ -1,30 +1,41 @@
-﻿Namespace Controls
+﻿Imports System.Windows.Forms
+
+Namespace Controls
     Public Class ResumenMesaViewControl
         Dim _cliente As New Logica.Cliente
-        Private Sub TableLayoutPanel_ResumenContainer_Paint(sender As Object, e As EventArgs) _
-            Handles MyBase.Paint
 
+        Public Sub ResumenMesaViewControl_Load(sender As Object, e As EventArgs) Handles Me.Load
+            UpdateControls()
+        End Sub
+
+        Private Sub UpdateControls()
             Dim row As Integer = 0
             Dim col As Integer = 0
             Dim count As Integer = 0
-            Dim layout As New Windows.Forms.TableLayoutPanel With {.ColumnCount = 4}
-
-            Me.TableLayoutPanel_ResumenContainer.Controls.Add(layout, 0, 1)
             Dim listMesas As List(Of Models.MesaDTO) = _cliente.GetMesas()
-            For Each mesaDTO As Models.MesaDTO In listMesas
-                col = count Mod 4
-                Dim resumenMesa As New ResumenMesaControl With {.AutoSize = True}
-                resumenMesa.LabelControl_Garzon_Value.Text = mesaDTO.UsuarioID
-                resumenMesa.LabelControl_MesaN_Value.Text = mesaDTO.Numero
-                resumenMesa.LabelControl_Hora_Value.Text = mesaDTO.FechaCreacion.Value.TimeOfDay.ToString.Substring(0, 5)
-                layout.Controls.Add(resumenMesa, col, row)
+            If listMesas IsNot Nothing Then
 
-                If col = 3 Then
-                    row += 1
-                End If
-                count += 1
-            Next
+                Dim layout As New TableLayoutPanel With {.ColumnCount = 5,
+                                                                   .Dock = DockStyle.Fill,
+                                                                   .Name = "TableLayoutPanel_Resumen_Mesas",
+                                                                   .AllowDrop = True}
 
+                For Each mesaDTO As Models.MesaDTO In listMesas.OrderBy(Function(m) m.Numero)
+                    col = count Mod 5
+                    layout.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, CSng(100 / 5)))
+                    Dim resumenMesa As New ResumenMesaControl With {.Anchor = AnchorStyles.Top, .Dock = DockStyle.Fill}
+                    resumenMesa.LabelControl_Garzon_Value.Text = mesaDTO.UsuarioID
+                    resumenMesa.LabelControl_MesaN_Value.Text = mesaDTO.Numero
+                    resumenMesa.LabelControl_Hora_Value.Text = mesaDTO.FechaCreacion.Value.ToShortTimeString()
+                    layout.Controls.Add(resumenMesa, col, row)
+
+                    If col = 5 Then
+                        row += 1
+                    End If
+                    count += 1
+                Next
+                Me.TableLayoutPanel_ResumenContainer.Controls.Add(layout, 0, 1)
+            End If
         End Sub
     End Class
 End Namespace
